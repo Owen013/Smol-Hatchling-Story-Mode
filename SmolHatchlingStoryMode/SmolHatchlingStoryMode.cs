ef29900afd41883cd0bc3e1af7e43ba5d3c807d8
Owen_013 <owen@crawfordsix.com>
2023-12-06 16:23:15 -0500

    Add project files.

---
+++ SmolHatchlingStoryMode/SmolHatchlingStoryMode.cs
@@ -0,0 +1,32 @@
+﻿using OWML.Common;
+using OWML.ModHelper;
+
+namespace SmolHatchlingStoryMode
+{
+    public class SmolHatchlingStoryMode : ModBehaviour
+    {
+        // Config
+        public bool _storyEnabled;
+
+        // Other
+        public static SmolHatchlingStoryMode Instance;
+        public StoryController _storyController;
+
+        public override void Configure(IModConfig config)
+        {
+            base.Configure(config);
+        }
+
+        public void Awake()
+        {
+            Instance = this;
+            gameObject.AddComponent<StoryController>();
+        }
+
+        public void Start()
+        {
+            // Ready
+            ModHelper.Console.WriteLine($"Smol Hatchling Story Mode is ready to go!", MessageType.Success);
+        }
+    }
+}
---
+++ SmolHatchlingStoryMode/StoryController.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using System.Xml;
+using UnityEngine;
+using SmolHatchling;
+
+namespace SmolHatchlingStoryMode
+{
+    public class StoryController : MonoBehaviour
+    {
+        public static StoryController Instance;
+        public bool _busted;
+
+        public void Awake()
+        {
+            Instance = this;
+            Harmony.CreateAndPatchAll(typeof(StoryController));
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(CharacterDialogueTree), nameof(CharacterDialogueTree.EndConversation))]
+        public static void EndConversation()
+        {
+            if (DialogueConditionManager.s_instance.GetConditionState("Busted") && !Instance._busted)
+            {
+                Instance._busted = true;
+                Locator.GetShipBody().GetComponentInChildren<ShipCockpitController>().LockUpControls(Mathf.Infinity);
+                NotificationManager.s_instance.PostNotification(new NotificationData(NotificationTarget.Player, "SHIP HAS BEEN DISABLED BY GROUND CONTROL", 5f), false);
+                NotificationManager.s_instance.PostNotification(new NotificationData(NotificationTarget.Ship, "SHIP DISABLED"), true);
+            }
+        }
+    }
+}
